bd36e8dd8e3ca11060e51dfd63c6541782b12801
kiboma <omayioben@gmail.com>
2024-02-19 08:00:16 +0300

    Add routes to the pages

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,12 +1,17 @@
 import { Route,Routes } from "react-router-dom"
-
+import { Home,Login,Register,Reserved,BookDetail } from "../pages"
 
 
 export const AllRoutes = () => {
   return (
     <Routes>
-        <Route> </Route>
-      
+        <Route path="/" element={<Home />}  />
+        <Route path="/reserved" element={<Reserved />}  />
+        <Route path="/login" element={<Login />}  />
+        <Route path="/register" element={<Register />}  />
+        <Route path="/book/:id" element={<BookDetail />}  />
+        <Route path="/register" element={<PageNotFound />}  />
+
     </Routes>
   )
 }

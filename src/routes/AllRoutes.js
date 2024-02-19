a8830cd4fdf1c3d76b062d59644fc8890cad4470
kiboma <omayioben@gmail.com>
2024-02-19 08:03:40 +0300

    Add dynamic titles

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,12 +5,12 @@ import { Home,Login,Register,Reserved,BookDetail,PageNotFound } from "../pages"
 export const AllRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />}  />
-        <Route path="/reserved" element={<Reserved />}  />
-        <Route path="/login" element={<Login />}  />
-        <Route path="/register" element={<Register />}  />
+        <Route path="/" element={<Home title="Home" />}  />
+        <Route path="/reserved" element={<Reserved title="Reserved" />}  />
+        <Route path="/login" element={<Login title="Login" />}  />
+        <Route path="/register" element={<Register title="Register" />}  />
         <Route path="/book/:id" element={<BookDetail />}  />
-        <Route path="/register" element={<PageNotFound />}  />
+        <Route path="/register" element={<PageNotFound title="Page Not Found" />}  />
 
     </Routes>
   )

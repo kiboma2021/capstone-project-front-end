1cee431356f9b8773b3a287a434456bcc7494b62
kiboma <omayioben@gmail.com>
2024-02-20 15:20:09 +0300

    Add BookCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
+export { BookCard } from './BookCard';
+// eslint-disable-next-line import/no-cycle
 export { NavPanel } from './NavPanel';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,19 @@
 /* eslint-disable import/prefer-default-export */
 
 import useFetch from '../hooks/useFetch';
+// eslint-disable-next-line import/no-cycle
+import { BookCard } from '../components';
 
 export const Home = () => {
   const url = 'http://localhost:8000/books';
   const { data } = useFetch(url);
-  // eslint-disable-next-line no-console
-  console.log(data);
   return (
     <div>
-      <p>Main Page</p>
+      {data && data.map((book) => (
+        <div key={book.id}>
+          <BookCard book={book} />
+        </div>
+      ))}
     </div>
   );
 };

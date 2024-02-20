6ae308a8fdcbcaddd4932599065638bad187efed
kiboma <omayioben@gmail.com>
2024-02-20 09:06:18 +0300

    Add books to Home Page

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,8 +7,6 @@ const useFetch = (url) => {
     async function fetchBooks() {
       const response = await fetch(url);
       const result = await response.json();
-      // eslint-disable-next-line no-console
-      console.log(result);
       setData(result);
     }
     fetchBooks();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 
-export const Home = () => (
-  <div>
-    <p>Main Page</p>
-  </div>
-);
+import useFetch from '../hooks/useFetch';
+
+export const Home = () => {
+  const url = 'http://localhost:8000/books';
+  const { data } = useFetch(url);
+  // eslint-disable-next-line no-console
+  console.log(data);
+  return (
+    <div>
+      <p>Main Page</p>
+    </div>
+  );
+};

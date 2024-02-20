9bc3490d1bed3f32a11b6107663a91f91dd6a018
kiboma <omayioben@gmail.com>
2024-02-20 16:19:49 +0300

    Add social icons

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -17,12 +17,18 @@ export const BookCard = ({ book }) => {
         </Link>
         <div className="text-3xl text-gray-400">.........................</div>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        <Link to="/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Read more
-          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-          </svg>
-        </Link>
+
+        <div className="flex justify-center gap-5">
+          <a href="/">
+            <i className="bi bi-facebook">.</i>
+          </a>
+          <a href="/">
+            <i className="bi bi-twitter-x">.</i>
+          </a>
+          <a href="/">
+            <i className="bi bi-instagram">.</i>
+          </a>
+        </div>
       </div>
     </div>
   );

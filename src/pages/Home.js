ae59aeaacec0519816c9e09192af54496ba58cce
kiboma <omayioben@gmail.com>
2024-02-21 08:44:01 +0300

    Style Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,19 @@ export const Home = () => {
   const url = 'http://localhost:8000/books';
   const { data } = useFetch(url);
   return (
-    <div className="flex flex-wrap gap-5 justify-center">
-      {data && data.map((book) => (
-        <div key={book.id}>
-          <BookCard book={book} />
-        </div>
-      ))}
+    <div>
+      <div className="flex flex-col gap-5 text-center mb-5">
+        <p className="text-2xl font-bold">ALL BOOKS IN STORE</p>
+        <p className="text-gray-400">Please select a book for more details</p>
+        <div className="text-3xl text-gray-400">.........................</div>
+      </div>
+      <div className="flex flex-wrap gap-5 justify-center">
+        {data && data.map((book) => (
+          <div key={book.id}>
+            <BookCard book={book} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

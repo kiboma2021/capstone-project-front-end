1ecfc70ebc8724e6c87f75f875a365d97e0b52c4
kiboma <omayioben@gmail.com>
2024-02-22 19:20:14 +0300

    Update home page with link to All and featured books

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable import/no-cycle */
+import { Link } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import { FeaturedBooks } from '../components';
 
@@ -13,6 +14,25 @@ export const Home = () => {
         <div>
           <FeaturedBooks data={data} />
         </div>
+      </div>
+      <div className="flex flex-wrap justify-center mt-">
+        <Link to="/books">
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <svg className="w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 20l4-16m2 16l4-16M6 9h14m-4 0H8" />
+            </svg>
+            View All Books
+          </button>
+
+        </Link>
+        <Link to="/featured">
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            View All Featured Books
+            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </button>
+        </Link>
       </div>
 
     </div>

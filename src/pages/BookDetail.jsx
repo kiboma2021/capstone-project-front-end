eaab6e504f4adfff856d2b5c61c23a84eee9d278
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-02-25 17:17:14 -0500

    style details page

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -19,12 +19,45 @@ export default function BookDetail() {
   if (!book) return <div>Loading...</div>;
 
   return (
-    <div className="content private">
-      <img alt="Book cover" src={book.image_url} />
-      <p>{book.id}</p>
-      <p>{book.title}</p>
-      <p>{book.price}</p>
-      <button className="btn" type="button" onClick={goToReservations}>RESERVE</button>
+    <div className="content details">
+      <img alt="Book cover" src={book.image_url} className="image-details" />
+      <div className="details-container">
+        <h1>BOOK DETAILS</h1>
+        <div className="details-book">
+          <div>
+            <div>Id:</div>
+            <div>{book.id}</div>
+          </div>
+          <div>
+            <div>Title:</div>
+            <div>{book.title}</div>
+          </div>
+          <div>
+            <div>Price:</div>
+            <div>{book.price}</div>
+          </div>
+          <div>
+            <div>Description:</div>
+            <div>{book.description}</div>
+          </div>
+          <div>
+            <div>Rating:</div>
+            <div>
+              {book.rating}
+              /5
+            </div>
+          </div>
+          <div>
+            <div>Year:</div>
+            <div>{book.year}</div>
+          </div>
+          <div>
+            <div>Author:</div>
+            <div>{book.author}</div>
+          </div>
+          <button className="btn details-btn" type="button" onClick={goToReservations}>RESERVE</button>
+        </div>
+      </div>
     </div>
   );
 }
